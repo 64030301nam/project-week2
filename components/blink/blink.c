ae19531aadf616862483f0b436adee4ccad3fe6f
64030301nam <64030301@kmitl.ac.th>
2023-11-11 23:17:46 +0700

    Update blink.c

--- components/blink/blink.c
+++ components/blink/blink.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "driver/gpio.h"
 #include "blink.h"
 
-void func(void)
+void blink(int pin)
 {
-
+	gpio_set_level(pin, 1);
+	usleep(500000);
+	gpio_set_level(pin, 0);
+	usleep(500000);
 }
